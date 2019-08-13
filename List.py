3495a8bc524e8b1825458614685118c5bffc95d8
nick <Lznick.zhang@gmail.com>
2019-08-13 18:26:44 +1200

    List

--- List.py
+++ List.py
@@ -1,7 +1,7 @@
 
-List----------------------------------
+# List----------------------------------
 
-#append, extend, insert
+# append, extend, insert
 member = ["nick", "zhang"]
 print(member)
 
@@ -17,7 +17,7 @@ print(member)
 member.insert(0, "ucol")
 print(member)
 
-#remove, del, pop
+# remove, del, pop
 member = ["nick", "zhang", "athar", "ucol"]
 member.remove("athar")
 print(member)
@@ -28,7 +28,7 @@ print(member)
 print(member.pop(0))
 
 
-#slice
+# slice
 member = ["nick", "zhang", "athar", "ucol"]
 print(member[1:3])
 print(member)
@@ -40,7 +40,7 @@ member2 = member[0:4]
 print(member2)
 
 
-#count, index, reverse, sort
+# count, index, reverse, sort
 member = ["nick", "zhang", "athar", "ucol"]
 
 print(member.count("nick"))
