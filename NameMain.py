0e2636f963c97a3c84aeb2a662db0fdf19bfdcda
nick <Lznick.zhang@gmail.com>
2019-08-31 22:19:33 +1200

    NameMain

---
+++ NameMain.py
@@ -0,0 +1,20 @@
+# This is a temperature conversion
+def c2f(cel):
+    fah = cel * 2 + 32
+    return fah
+
+
+def f2c(fah):
+    cel = (fah - 32) / 2
+    return cel
+
+
+def test():
+    print("test, 0 Celsius = %.2f Fahrenheit" % c2f(0))
+    print("test, 0 Fahrenheit = %.2f Celsius" % f2c(0))
+
+
+# If this is main programme then proceduce function test
+# otherwise call others function
+if __name__ == "__main__":
+    test()
