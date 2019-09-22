85b50eadbfd6cc9391d2c161081f638d4e9cbc32
NickZhang624 <53941060+NickZhang624@users.noreply.github.com>
2019-09-22 19:55:05 +1200

    File split(Task)

--- FileSplit.py
+++ FileSplit.py
@@ -1,14 +1,3 @@
-# Jesse: In the months leading up to my wedding, I was thinking about you all the time. I mean, even on my way there; I’m in the car, a buddy of mine is driving me downtown and I’m staring out the window, and I think I see you, not far from the church, right? Folding up an umbrella and walking into a deli on the corner of 13th and Broadway. And I thought I was going crazy, but now I think it probably was you.
-# Celine: I lived on 11th and Broadway.
-# Jesse: You see?
-# ============================================
-# Mark Zuckerberg: As for any charges stemming from the breach of security, I believe I deserve some recognition from this board.
-# Chairwoman: I’m sorry?
-# Mark Zuckerberg: Yes?
-# Chairwoman: I don’t understand.
-# Mark Zuckerberg: Which part?
-
-
 # This is task to split above movie dialogue
 # No.1 Save dialogue to txt.file separately according to different people
 # No.2 Save two dialogues to txt.file which split by "========"
