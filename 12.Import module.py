76b4d79d9f73da870513522117d5207988467596
NickZhang624 <53941060+NickZhang624@users.noreply.github.com>
2019-11-06 15:48:41 +1300

    12.Import module.py

---
+++ 12.Import module.py
@@ -0,0 +1,9 @@
+#This is twelfth topic about Python Module.
+#Consider a module to be the same as a code library.
+#A file containing a set of functions you want to include in your application.
+
+#improt other module created in this Python file before
+import NameMain as tc
+
+print("32 Celsius = %.2f Fahrenheit" % tc.c2f(32))
+print("99 Fahrenheit = %.2f Celsius" % tc.f2c(99))
--- If name...(import module).py
+++
@@ -1,4 +0,0 @@
-import NameMain as tc
-
-print("32 Celsius = %.2f Fahrenheit" % tc.c2f(32))
-print("99 Fahrenheit = %.2f Celsius" % tc.f2c(99))
