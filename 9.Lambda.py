5620487d21d783118f3168c6a9b5c532ed92cd79
NickZhang624 <53941060+NickZhang624@users.noreply.github.com>
2019-11-06 14:17:59 +1300

    Update 9.Lambda.py

--- 9.Lambda.py
+++ 9.Lambda.py
@@ -1,3 +1,8 @@
+# This is ninth part to introduce Python function
+# A function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function. A function can return data as a result.
+
+# In Python a function is defined using the def keyword:
+
 g = lambda x: 2*x +1
 
 print(g(5))
