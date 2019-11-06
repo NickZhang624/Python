76f302927b36a6a62598380594fdc4905ab61a14
NickZhang624 <53941060+NickZhang624@users.noreply.github.com>
2019-11-06 15:42:33 +1300

    11.Inheritance.py

--- 11.Inheritance.py
+++ 11.Inheritance.py
@@ -1,3 +1,9 @@
+#This is eleventh topic about Python class and objects.
+#Inheritance allows us to define a class that inherits all the methods and properties from another class.
+#Parent class is the class being inherited from, also called base class.
+#Child class is the class that inherits from another class, also called derived class.
+
+#import function allows internal Python library - random
 import random as r
 
 class Parent:
